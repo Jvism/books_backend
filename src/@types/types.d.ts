f64f4d52eaa4ed63136b08986ebb6458586a3910
ji <jvi.smo@gmail.com>
2023-10-23 23:13:20 -0300

    types: add types new modules

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,4 +1,5 @@
 import type { User } from '@prisma/client'
+import type { BookStatus } from '@prisma/client'
 
 export interface IUserCreate {
   username: string
@@ -25,6 +26,48 @@ export interface IUserSimpleResponse {
   lastname: string
   username: string
   email: string
+}
+
+export interface IUserResponse {
+  id: number
+  username: string
+  email: string
+  name: string | null
+  lastname: string | null
+  instagram: string | null
+  linkedin: string | null
+  createdAt: Date
+  updatedAt: Date | null
+}
+
+// BOOKS
+
+export interface IBookResponse {
+  id: number
+  title: string
+  status: BookStatus
+  publishDate: Date
+  shortDescription: string | null
+  longDescription: string | null
+  thumbnailUrl: string | null
+  pages: number | null
+  createdAt: Date
+  updatedAt: Date | null
+}
+
+// AUTHORS
+
+export interface IAuthorResponse {
+  id: number
+  name: string
+  lastName: string
+  createdAt: Date
+  updatedAt: Date | null
+}
+
+export interface ICategoryResponse {
+  id: number
+  name: string
 }
 
 // JWT
